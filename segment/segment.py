900fde10f3a791e643a354b536ec443d79909a05
dibbo82 <Drewd.work@gmail.com>
2018-08-16 21:31:20 +1000

    new project- Added new wiring diagrams to assist with wiring devices

--- segment/segment.py
+++ segment/segment.py
@@ -1,41 +1,49 @@
 #!/usr/bin/env python
-
 import RPi.GPIO as GPIO
 import time
-
-pins = [11,12,13,15,16,18,22,7]
-dats = [0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x80]
-
-def setup():
-	GPIO.setmode(GPIO.BOARD)
-	for pin in pins:
-		GPIO.setup(pin, GPIO.OUT)   # Set pin mode as output
-		GPIO.output(pin, GPIO.LOW)
-
-def writeOneByte(val):
-	GPIO.output(11, val & (0x01 << 0))  
-	GPIO.output(12, val & (0x01 << 1))  
-	GPIO.output(13, val & (0x01 << 2))  
-	GPIO.output(15, val & (0x01 << 3))  
-	GPIO.output(16, val & (0x01 << 4))  
-	GPIO.output(18, val & (0x01 << 5))  
-	GPIO.output(22, val & (0x01 << 6))  
-	GPIO.output(7,  val & (0x01 << 7)) 
-
-def loop():
-	while True:
-		for dat in dats:
-			writeOneByte(dat)
-			time.sleep(0.5)
-
-def destroy():
-	for pin in pins:
-		GPIO.output(pin, GPIO.LOW)
-	GPIO.cleanup()             # Release resource
-
-if __name__ == ('__main__'):     # Program start from here
-	setup()
-	try:
-		loop()
-	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be executed.
-		destroy()
+GPIO.setmode(GPIO.BCM)
+ 
+# GPIO ports for the 7seg pins
+segments =  (11,4,23,8,7,10,18,25)
+# 7seg_segment_pins (11,7,4,2,1,10,5,3) +  100R inline
+ 
+for segment in segments:
+    GPIO.setup(segment, GPIO.OUT)
+    GPIO.output(segment, 0)
+ 
+# GPIO ports for the digit 0-3 pins 
+digits = (22,27,17,24)
+# 7seg_digit_pins (12,9,8,6) digits 0-3 respectively
+ 
+for digit in digits:
+    GPIO.setup(digit, GPIO.OUT)
+    GPIO.output(digit, 1)
+ 
+num = {' ':(0,0,0,0,0,0,0),
+    '0':(1,1,1,1,1,1,0),
+    '1':(0,1,1,0,0,0,0),
+    '2':(1,1,0,1,1,0,1),
+    '3':(1,1,1,1,0,0,1),
+    '4':(0,1,1,0,0,1,1),
+    '5':(1,0,1,1,0,1,1),
+    '6':(1,0,1,1,1,1,1),
+    '7':(1,1,1,0,0,0,0),
+    '8':(1,1,1,1,1,1,1),
+    '9':(1,1,1,1,0,1,1)}
+ 
+try:
+    while True:
+        n = time.ctime()[11:13]+time.ctime()[14:16]
+        s = str(n).rjust(4)
+        for digit in range(4):
+            for loop in range(0,7):
+                GPIO.output(segments[loop], num[s[digit]][loop])
+                if (int(time.ctime()[18:19])%2 == 0) and (digit == 1):
+                    GPIO.output(25, 1)
+                else:
+                    GPIO.output(25, 0)
+            GPIO.output(digits[digit], 0)
+            time.sleep(0.001)
+            GPIO.output(digits[digit], 1)
+finally:
+    GPIO.cleanup()
