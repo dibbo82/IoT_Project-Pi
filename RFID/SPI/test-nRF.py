56b6327980c8f6ec2a8ceff19671988bd86da9a9
dibbo82 <Drewd.work@gmail.com>
2018-08-26 09:12:00 +1000

    New Project- modified RFID/MFRC522.py file and Fixed SPI programs but C extension not working

--- RFID/SPI/test-nRF.py
+++ RFID/SPI/test-nRF.py
@@ -9,12 +9,12 @@ import spi
 
 
 status = spi.openSPI(speed=1000000)
-print "SPI configuration = ", status
+print ("SPI configuration = "), status
 
-print "Reading nRF24L01 status registers:"
+print ("Reading nRF24L01 status registers:")
 
 for x in range(28):
 	dat = spi.transfer((x, 0))
-	print "nRF Register 0x%X: %X" % (x, dat[1])
+	print ("nRF Register 0x%X: %X") % (x, dat[1])
 
 spi.closeSPI()
--- RFID/SPI/test_multi.py
+++ RFID/SPI/test_multi.py
@@ -3,14 +3,14 @@
 import spi
 
 ret = spi.openSPI(speed=1000000)
-print "openSPI returns: ", ret
+print ("openSPI returns: "), ret
 fd = ret ["fd"]
-print "fd = ", fd
+print ("fd = "), fd
 
-print "Reading nRF24L01 status registers:"
+print ("Reading nRF24L01 status registers:")
 
 for x in range(28):
 	dat = spi.transfer(fd, (x, 0))
-	print "nRF Register 0x%X: %X" % (x, dat[1])
+	print ("nRF Register 0x%X: %X")% (x, dat[1])
 
 spi.closeSPI (fd)
